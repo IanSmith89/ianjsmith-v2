95003fe5dbd9dfef27f8de9db06479bf621172e2
Ian Smith <iansmith1026@gmail.com>
2017-09-23 17:12:25 -0700

    test

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -14,7 +14,7 @@ export default class WorkPage extends React.Component {
             <main className="home page">
                 <div className="container">
                     <div className="row">
-                        <div className="twelve columns">Work Page Staging Test</div>
+                        <div className="twelve columns">Work Page Staging Test Again</div>
                     </div>
                 </div>
             </main>
