a9e18f74a7024bd6fd88ecc306f56c2d534841ea
Ian Smith <iansmith1026@gmail.com>
2018-10-01 14:56:11 -0700

    update webpack build to include favicon

--- src/index.js
+++ src/index.js
@@ -1,50 +1,51 @@
-require('./assets/styles/main.scss');
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'mobx-react';
-import App from './components/App';
+import "./assets/styles/main.scss";
+import "../favicon.ico";
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "mobx-react";
+import App from "./components/App";
 
-import appStore from './stores/appStore';
-import workStore from './stores/workStore';
+import appStore from "./stores/appStore";
+import workStore from "./stores/workStore";
 
 const stores = {
-    appStore,
-    workStore
+  appStore,
+  workStore
 };
 
-import * as appActions from './actions/appActions';
-import * as workActions from './actions/workActions';
+import * as appActions from "./actions/appActions";
+import * as workActions from "./actions/workActions";
 
 const actions = {
-    ...appActions,
-    ...workActions
+  ...appActions,
+  ...workActions
 };
 
 ReactDOM.render(
-    <AppContainer>
-        <Router>
-            <Provider {...stores}>
-                <App stores={stores} actions={actions} />
-            </Provider>
-        </Router>
-    </AppContainer>,
-    document.getElementById('root')
+  <AppContainer>
+    <Router>
+      <Provider {...stores}>
+        <App stores={stores} actions={actions} />
+      </Provider>
+    </Router>
+  </AppContainer>,
+  document.getElementById("root")
 );
 
 // Hot Module Replacement API
 if (module.hot) {
-    module.hot.accept('./components/App', () => {
-        ReactDOM.render(
-            <AppContainer>
-                <Router>
-                    <Provider {...stores}>
-                        <App stores={stores} actions={actions} />
-                    </Provider>
-                </Router>
-            </AppContainer>,
-            document.getElementById('root')
-        );
-    });
+  module.hot.accept("./components/App", () => {
+    ReactDOM.render(
+      <AppContainer>
+        <Router>
+          <Provider {...stores}>
+            <App stores={stores} actions={actions} />
+          </Provider>
+        </Router>
+      </AppContainer>,
+      document.getElementById("root")
+    );
+  });
 }
