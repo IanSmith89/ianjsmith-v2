a4531f87d8128f3640588f951a53130241d48feb
Ian Smith <iansmith1026@gmail.com>
2018-10-02 14:07:58 -0700

    fixed prettier

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,72 +1,74 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { observer } from "mobx-react";
-import cx from "classnames";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { observer } from 'mobx-react';
+import cx from 'classnames';
 
 @observer
 export default class Nav extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.actions = props.actions;
-    this.historyAction = props.history.action;
-  }
+		this.actions = props.actions;
+		this.historyAction = props.history.action;
+	}
 
-  componentWillMount() {
-    const { pathname } = this.props.location;
-    this.actions.pageIsActive(pathname);
-  }
+	componentWillMount() {
+		const { pathname } = this.props.location;
+		this.actions.pageIsActive(pathname);
+	}
 
-  componentWillReceiveProps(nextProps) {
-    const { pathname } = nextProps.location;
-    this.actions.pageIsActive(pathname);
-  }
+	componentWillReceiveProps(nextProps) {
+		const { pathname } = nextProps.location;
+		this.actions.pageIsActive(pathname);
+	}
 
-  componentWillUpdate(nextProps) {
-    if (this.historyAction !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }
+	componentWillUpdate(nextProps) {
+		if (this.historyAction !== 'POP') {
+			window.scrollTo(0, 0);
+		}
+	}
 
-  render() {
-    const { activePathname } = this.props.stores.appStore;
+	render() {
+		const { activePathname } = this.props.stores.appStore;
 
-    return (
-      <nav
-        className={cx("top-nav", {
-          light: activePathname !== "/"
-        })}
-      >
-        <div className="container">
-          <div className="row">
-            <div className="twelve columns">
-              <div className="logo">
-                <Link to="/">Ian J. Smith</Link>
-              </div>
-              <div className="page-links">
-                <Link
-                  to="/"
-                  className={cx({
-                    active:
-                      activePathname === "/" ||
-                      activePathname.indexOf("/projects") !== -1
-                  })}
-                >
-                  Work
-                </Link>
-                <Link
-                  to="/about"
-                  className={cx({
-                    active: activePathname === "/about"
-                  })}
-                >
-                  About/Contact
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+		return (
+			<nav
+				className={cx('top-nav', {
+					light: activePathname !== '/'
+				})}
+			>
+				<div className="container">
+					<div className="row">
+						<div className="twelve columns">
+							<div className="logo">
+								<Link to="/">Ian J. Smith</Link>
+							</div>
+							<div className="page-links">
+								<Link
+									to="/"
+									className={cx({
+										active:
+											activePathname === '/' ||
+											activePathname.indexOf(
+												'/projects'
+											) !== -1
+									})}
+								>
+									Work
+								</Link>
+								<Link
+									to="/about"
+									className={cx({
+										active: activePathname === '/about'
+									})}
+								>
+									About/Contact
+								</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		);
+	}
 }
