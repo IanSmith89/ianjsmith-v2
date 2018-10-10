41e938c82e0dfa6f5e82e8986555276b99f1f9ca
Ian Smith <iansmith1026@gmail.com>
2018-10-10 00:24:04 -0700

    required babel-polyfill

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
+require('babel-core/polyfill');
+
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config({ silent: true });
 }
-
-console.log(process.env);
 
 const path = require('path');
 const fs = require('fs');
