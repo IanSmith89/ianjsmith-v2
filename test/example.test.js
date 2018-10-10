b96dd5fd79658af81f90fccc6b06d81174282772
Ian Smith <iansmith1026@gmail.com>
2018-10-09 22:04:50 -0700

    updated procfile script

--- test/example.test.js
+++ test/example.test.js
@@ -2,51 +2,12 @@
 
 process.env.NODE_ENV = 'test';
 
-const { suite, test } = require('mocha');
-const app = require('../server/server');
-const { knex } = require('../orm');
-const supertest = require('supertest');
+var assert = require('assert');
 
-// suite('Example Test', () => {
-// 	before(done => {
-// 		knex.migrate
-// 			.latest()
-// 			.then(() => {
-// 				done();
-// 			})
-// 			.catch(err => {
-// 				done(err);
-// 			});
-// 	});
-//
-// 	beforeEach(done => {
-// 		knex.seed
-// 			.run()
-// 			.then(() => {
-// 				done();
-// 			})
-// 			.catch(err => {
-// 				done(err);
-// 			});
-// 	});
-//
-// 	test('This example test returns to correct data', done => {
-// 		supertest(app)
-// 			.get('/api/example')
-// 			.set('Accept', 'application/json')
-// 			.expect(res => {
-// 				for (const item of res.body) {
-// 					delete item.createdAt;
-// 					delete item.updatedAt;
-// 				}
-// 			})
-// 			.expect(200, [
-// 				{ id: 1, name: 'John' },
-// 				{ id: 2, name: 'Paul' },
-// 				{ id: 3, name: 'George' },
-// 				{ id: 4, name: 'Ringo' }
-// 			])
-// 			.expect('Content-Type', /json/)
-// 			.end(done);
-// 	});
-// });
+describe('Array', function() {
+	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', function() {
+			assert.equal([1, 2, 3].indexOf(4), -1);
+		});
+	});
+});
