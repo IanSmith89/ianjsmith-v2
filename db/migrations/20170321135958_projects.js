88f9e7c84a1530f4c4e9eff177101ef6b65fa15c
Ian Smith <iansmith1026@gmail.com>
2019-01-27 18:50:55 -0800

    added new case studies content

--- db/migrations/20170321135958_projects.js
+++ db/migrations/20170321135958_projects.js
@@ -4,16 +4,21 @@ exports.up = function(knex) {
 	return knex.schema.createTable('projects', table => {
 		table.string('id').notNullable();
 		table.string('name').notNullable();
-		table.string('client');
-		table.text('description', 'mediumtext').notNullable();
-		table.specificType('how', 'text[]');
-		table.specificType('services', 'text[]').notNullable();
-		table.specificType('takeaways', 'text[]');
-		table.specificType('tech_stack', 'text[]');
-		table.specificType('users', 'text[]');
-		table.string('website_url');
-		table.specificType('why', 'text[]');
-		table.string('with').notNullable();
+		// table.string('client');
+		// table.text('description', 'mediumtext').notNullable();
+		table.specificType('design_process', 'text[]');
+		// table.specificType('how', 'text[]');
+		table.specificType('outcome_and_reflection', 'text[]');
+		table.string('problem');
+		// table.specificType('services', 'text[]').notNullable();
+		table.string('solution');
+		// table.specificType('takeaways', 'text[]');
+		table.string('team_and_role');
+		// table.specificType('tech_stack', 'text[]');
+		// table.specificType('users', 'text[]');
+		// table.string('website_url');
+		// table.specificType('why', 'text[]');
+		// table.string('with').notNullable();
 	});
 };
 
