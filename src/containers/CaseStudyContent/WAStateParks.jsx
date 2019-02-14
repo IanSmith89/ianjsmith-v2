a903057e099b316a65105612ff9d8661245e76a8
Ian Smith <iansmith1026@gmail.com>
2019-02-13 21:11:32 -0800

    updated WA State Parks page

--- src/containers/CaseStudyContent/WAStateParks.jsx
+++ src/containers/CaseStudyContent/WAStateParks.jsx
@@ -5,7 +5,20 @@ import ImageCard from '../../components/ImageCard';
 @inject('actions', 'lightboxStore')
 @observer
 export default class WAStateParks extends React.Component {
-	images = [];
+	images = [
+		{
+			caption: <p className="image-lightbox-caption">Sketches</p>,
+			src: require('../../assets/images/wasp-sketches-alt.jpg')
+		},
+		{
+			caption: <p className="image-lightbox-caption">Wireframes</p>,
+			src: require('../../assets/images/wasp-wires.jpg')
+		},
+		{
+			caption: <p className="image-lightbox-caption">Final Mockups</p>,
+			src: require('../../assets/images/wasp-mocks.jpg')
+		}
+	];
 
 	componentDidMount() {
 		this.props.actions.setLightboxImages(
@@ -27,6 +40,89 @@ export default class WAStateParks extends React.Component {
 					<div className="twelve columns">
 						<h3 className="section-label">Design Process</h3>
 						<div className="underline" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<h3 className="section-label">User Research</h3>
+						<div className="underline" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<p className="section-content full">
+							{project.designProcess[0]}
+						</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<h3 className="section-label">Sketches</h3>
+						<div className="underline" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="six columns">
+						<ImageCard
+							alt="WA State Parks sketches"
+							onClick={() =>
+								actions.openLightbox(lightboxStore, 0)
+							}
+							src={this.images[0].src}
+						/>
+					</div>
+					<div className="six columns">
+						<p className="section-content">
+							{project.designProcess[1]}
+						</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<h3 className="section-label">Wireframes</h3>
+						<div className="underline" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<ImageCard
+							alt="WA State Parks wireframes"
+							onClick={() =>
+								actions.openLightbox(lightboxStore, 1)
+							}
+							src={this.images[1].src}
+						/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<p className="section-content full">
+							{project.designProcess[2]}
+						</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<h3 className="section-label">Final Mockups</h3>
+						<div className="underline" />
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<ImageCard
+							alt="WA State Parks mockups"
+							onClick={() =>
+								actions.openLightbox(lightboxStore, 2)
+							}
+							src={this.images[2].src}
+						/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="twelve columns">
+						<p className="section-content full">
+							{project.designProcess[3]}
+						</p>
 					</div>
 				</div>
 			</div>
